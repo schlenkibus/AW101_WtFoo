241465bf4cd6cd95c5362f5e2f238724d8fdb6fa
schlenkibus <justus.scheil@parteimail.de>
2019-10-26 11:15:21 +0200

    started implementing plugin rework

--- examples/ModularPlayground/src/main.cpp
+++ examples/ModularPlayground/src/main.cpp
@@ -8,32 +8,32 @@
 #include <thread>
 #include <HAL/HAL/HAL.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
-  ArgumentParser parser({ "docroot", "http-listen", "module-path", "hal-enable" }, argc, argv);
+    ArgumentParser parser({"docroot", "http-listen", "module-path", "hal-enable"}, argc, argv);
 
-  ModularPlaygroundApplication application;
+    ModularPlaygroundApplication application;
 
-  auto hostnameWithPort = parser.getArgumentValue("http-listen");
-  auto hostname = hostnameWithPort.substr(0, hostnameWithPort.find(':'));
-  std::unique_ptr<HAL> hal{nullptr};
+    auto hostnameWithPort = parser.getArgumentValue("http-listen");
+    auto hostname = hostnameWithPort.substr(0, hostnameWithPort.find(':'));
+    std::unique_ptr<HAL> hal{nullptr};
 
-  if(parser.parseBooleanArgument("hal-enable"))
-    hal = std::make_unique<HAL>(&application, hostname);
+    if (parser.parseBooleanArgument("hal-enable"))
+        hal = std::make_unique<HAL>(&application, hostname);
 
-  Directory d(parser.getArgumentValue("module-path"));
-  Directory h(parser.getArgumentValue("hardware-path"));
-  loadPlugins(&application, hal.get(), d, h);
+    Directory d(parser.getArgumentValue("module-path"));
+    loadDSPModules(&application, d);
+    Directory h(parser.getArgumentValue("hardware-path"));
+    loadHardwareModules(hal.get(), h);
 
-  auto webUI = std::thread([&]() {
-    return Wt::WRun(argc, argv, [&](const auto &env) {
-      return std::make_unique<ModularWebUI>(env, application);
+    auto webUI = std::thread([&]() {
+        return Wt::WRun(argc, argv, [&](const auto &env) {
+            return std::make_unique<ModularWebUI>(env, application);
+        });
     });
-  });
 
-  while(application.running())
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    while (application.running())
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  return 0;
+    return 0;
 }
